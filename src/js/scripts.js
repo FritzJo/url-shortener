5785c6a9823bf4c264d851152fc504b27e7b5e6f
FritzJo <fritzjo-git@mailbox.org>
2019-09-03 18:50:23 +0200

    formating

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,23 @@
-function shrink_button_click(){
-	var div = document.getElementById('loading-div');
-	div.style.display = 'block';
-    console.log("Processing...");
-	console.log(shrink_url());
-	div.style.display = 'none';
+function shrink_button_click() {
+    //Show loading bar
+    var div = document.getElementById('loading-div');
+    div.style.display = 'block';
+
+    //calculate and set results
+    var current_url = document.getElementById('icon_prefix').value
+    var s_url = shrink_url(current_url)
+    document.getElementById('s-url').innerHTML = s_url;
+    document.getElementById('t-url').innerHTML = current_url;
+    document.getElementById('date-url').innerHTML = "2020-02-20";
+    //Hide loading bar
+    div.style.display = 'none';
+
+    //Show results
+    div = document.getElementById('result-div');
+    div.style.display = 'block';
 }
 
-function shrink_url(url){
-	//TODO
-	var current_url = document.getElementById('icon_prefix').value
-	return current_url
+function shrink_url(url) {
+    //TODO
+    return url
 }
