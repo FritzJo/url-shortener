b29499a1671d12368369a4d75947f7185074c684
FritzJo <fritzjo-git@mailbox.org>
2019-11-30 23:31:17 +0100

    removed old code snippet

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -19,9 +19,6 @@ window.onload = function () {
     var parameter = window.location.href;
     if (parameter.includes("target")) {
         var url = new URL(parameter);
-        //document.addEventListener('turbolinks:load', () => {
-        //    M.updateTextFields();
-        //});
         document.getElementById("text_label").classList.add("active");
         document.getElementById("icon_prefix").value = url.searchParams.get("target");
         div = document.getElementById("result-div");
